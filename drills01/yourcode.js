11e730836f6327ef7906342c27c070e869fe6747
Samantha  Durant <samanthadurant11@Samanthas-MacBook-Air.local>
2019-11-25 15:08:12 -0800

    did more drills

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -7,7 +7,23 @@ function sumArray(arr){
   return total;
 }
 
-function fitWithinVal(){
+function fitWithinVal(arr, val){
+  //declare a number variable to add values to in order to check
+  //instantiate an array to push the nums that fit into
+  //loop through the array and check if each number can be added to the total without going over val amt
+    //if it can be added, add it to the total and push that value into numsThatFitArr
+    //if not, move on until loop is finished
+    //return numsThatFitArr
+
+  var numsThatFitArr = [];
+  var total = 0;
+  for (var i = 0; i < arr.length; i++){
+    if(arr[i] + total <= val){
+      total += arr[i];
+      numsThatFitArr.push(arr[i]);
+    }
+  }
+  return numsThatFitArr;
 
 }
 
